6876f7c8f13efc11364389e4320d2bfc28ade694
iKiru-hub <dkirubel@gmail.com>
2026-04-01 03:48:56 +0200

    quick update

--- src/evolution_dir/_utils_ev.py
+++ src/evolution_dir/_utils_ev.py
@@ -47,16 +47,15 @@ def load_autoencoder(index: int=0) -> dict:
     """ load the session of an autoencoder with its metadata """
 
     info, autoencoder = models.load_session(idx=index, verbose=False)
-    info = info["network_params"]
+    # info = info["network_params"]
 
     # get parameters
     W_ei_ca1, W_ca1_eo, B_ei_ca1, B_ca1_eo = autoencoder.get_weights(bias=True)
 
     print(info)
 
-    return {"dim_ei": info["dim_ei"],
-            "dim_ca1": info["dim_ca1"], "dim_eo": info["dim_eo"],
-            "K": info["K_ei"],
+    return {"dim_ei": info["dim_ei"], "dim_ca1": info["dim_ca1"],
+            "dim_eo": info["dim_eo"], "K_out": info["K"], "K_lat": info["K_lat"],
             "W_ei_ca1": W_ei_ca1, "W_ca1_eo": W_ca1_eo,
             "B_ei_ca1": B_ei_ca1, "B_ca1_eo": B_ca1_eo}
 
@@ -92,6 +91,21 @@ def exp_eval(data: np.ndarray, sigma: float):
 
     return out
 
+
+def _configure_genome(genome: list, genome_configs: dict):
+
+    result = {}
+    idx = 0
+    for i, (param, conf) in enumerate(genome_configs.items()):
+        if conf["active"]:
+            result[param] = np.clip(genome[idx], conf["min"], conf["max"])
+            idx += 1
+        else:
+            result[param] = conf["init"]
+
+    return result
+
+
 def evaluate_genome(genome: list, datasets: list, settings: dict):
 
     """
@@ -100,7 +114,7 @@ def evaluate_genome(genome: list, datasets: list, settings: dict):
     Parameters
     ----------
     genome: list
-        parameters for the model to evolve
+        parameters for the model to evolve : [0.2, -0.4, 0.4]
     datasets: list
         list of Dataloader objects
     settings: dict
@@ -114,10 +128,13 @@ def evaluate_genome(genome: list, datasets: list, settings: dict):
     assert "sigma" in tuple(settings.keys()), "no" + \
         " 'sigma' provided in settings"
     num_samples = settings["num_samples"]
+    genome_configs = settings["genome_configs"]
     W_ei_ca1 = settings["W_ei_ca1"]
     W_ca1_eo = settings["W_ca1_eo"]
     B_ei_ca1 = settings["B_ei_ca1"]
     B_ca1_eo = settings["B_ca1_eo"]
+    K_out = settings["K_out"]
+    K_lat = settings["K_lat"]
     num_reps = len(datasets)
 
     accuracy = np.zeros((num_reps, num_samples, num_samples))
@@ -129,17 +146,35 @@ def evaluate_genome(genome: list, datasets: list, settings: dict):
         for i in tqdm(range(num_samples), disable=True):
 
             # make model
-            model = models.MTL(W_ei_ca1=W_ei_ca1,
+            # model = models.MTL(W_ei_ca1=W_ei_ca1,
+            #                    W_ca1_eo=W_ca1_eo,
+            #                    B_ei_ca1=B_ei_ca1,
+            #                    B_ca1_eo=B_ca1_eo,
+            #                    dim_ca3=settings["dim_ei"],
+            #                    K_lat=int(np.clip(10*genome[0], MIN_VAL, MAX_VAL)),
+            #                    K_ca3=int(np.clip(10*genome[1], MIN_VAL, MAX_VAL)),
+            #                    K_out=int(np.clip(10*genome[2], MIN_VAL, MAX_VAL)),
+            #                    beta=abs(float(np.clip(100*genome[3],
+            #                                           MIN_VAL, MAX_VAL))),
+            #                    alpha=float(np.clip(genome[4], 0.01, 0.99)))
+
+            # make model
+            params = _configure_genome(genome=genome, genome_configs=genome_configs)
+            model = models.MTLev(W_ei_ca1=W_ei_ca1,
                                W_ca1_eo=W_ca1_eo,
+                               K_lat=params["K_lat"],
+                               K_out=params["K_out"],
+                               K_ca3=params["K_ca3"],
+                               dim_ca3=genome_configs["K_ca3"]["max"],
+                               beta_eo=params["beta_eo"],
+                               beta_is=params["beta_is"],
+                               beta_ca1=params["beta_ca1"],
+                               beta_ca3=params["beta_ca3"],
+                               alpha=params["alpha"],
+                               num_swaps_ca1=params["num_swaps_ca1"],
+                               num_swaps_ca3=params["num_swaps_ca3"],
                                B_ei_ca1=B_ei_ca1,
-                               B_ca1_eo=B_ca1_eo,
-                               dim_ca3=settings["dim_ei"],
-                               K_lat=int(np.clip(10*genome[0], MIN_VAL, MAX_VAL)),
-                               K_ca3=int(np.clip(10*genome[1], MIN_VAL, MAX_VAL)),
-                               K_out=int(np.clip(10*genome[2], MIN_VAL, MAX_VAL)),
-                               beta=abs(float(np.clip(100*genome[3],
-                                                      MIN_VAL, MAX_VAL))),
-                               alpha=float(np.clip(genome[4], 0.01, 0.99)))
+                               B_ca1_eo=B_ca1_eo)
 
             # train a dataset with pattern index 0.. i
             model.eval()
@@ -169,7 +204,9 @@ def evaluate_genome(genome: list, datasets: list, settings: dict):
     if np.any(np.isnan(result)):
         return 0.
 
-    return exp_eval(data=result, sigma=settings["sigma"]).mean()
+    score = exp_eval(data=result, sigma=settings["sigma"]).mean()
+    return score
+
 
 def save_genome(info: dict, name: str):
 
@@ -179,13 +216,17 @@ def save_genome(info: dict, name: str):
         json.dump(info, f)
 
 
-def load_genome(index: int):
+def load_genome(index: int=-2):
 
     """ load an evolved and saved genome """
 
     name = ""
-    path = os.path.abspath(__file__).split("src")[0] + \
-        "src/evolution/logs"
+    path = os.path.abspath(__file__).split("KAMemory")[0] + \
+        "KAMemory/src/evolution_dir/logs"
+    if index == -2:
+        print("available:")
+        for k, u in enumerate(os.listdir(path)): print(f"{k}:{u}")
+        index = int(input(f"index: "))
     for i, f in enumerate(os.listdir(path)):
         if int(f.split('_')[1]) == index:
             name = f"{path}/{f}"
@@ -200,5 +241,5 @@ def load_genome(index: int):
 
 
 if __name__ == "__main__":
-    file = load_genome(0)
+    file = load_genome(-2)
     print(f"loaded: {file}")
--- src/evolution_dir/evolution_search.py
+++ src/evolution_dir/evolution_search.py
@@ -11,12 +11,12 @@ import utils
 from logger import logger
 from _utils_ev import *
 
+print(os.getcwd())
 
-if "side_lab" in os.getcwd():
+if "doki" in os.getcwd():
     sys.path.append(os.getcwd().split("doki")[0] + \
         "doki/side_lab/evolution_cpp/core/cpp1/build")
     import evolution as ev
-
 elif "Research" in os.getcwd():
     sys.path.append(os.getcwd().split("Research")[0] + \
         "Research/studio/evolution_cpp/core/cpp1/build")
@@ -37,12 +37,73 @@ index=1 : K_ca3
 index=2 : K_out
 index=3 : beta
 index=4 : alpha
-
 """
 
 COLORS = ("red", "black", "blue", "green", "orange", "brown")
 GENOME_KEYS = ("K_lat", "K_ca3", "K_out", "beta", "alpha")
-SIGMA = 40
+SIGMA = 30
+
+GENOME_CONFIGS = {
+    "K_lat" : {"active": False,
+               "color": "red",
+               "init": 15,
+               "init": 15,
+               "min": 10,
+               "max": 100},
+    "K_out" : {"active": False,
+               "color": "blue",
+               "init": 5,
+               "min": 10,
+               "max": 100},
+    "K_ca3" : {"active": True,
+               "color": "black",
+               "init": 15,
+               "scale": 10,
+               "min": 1,
+               "max": 50},
+    "beta_eo"  : {"active": True,
+                  "color": "green",
+                  "init": 20,
+                  "scale": 100,
+                  "min": 0,
+                  "max": 400},
+    "beta_is"  : {"active": True,
+                  "color": "green",
+                  "init": 20.,
+                  "scale": 100,
+                  "min": 0,
+                  "max": 400},
+    "beta_ca1"  : {"active": True,
+                   "color": "green",
+                   "init": 100,
+                   "scale": 100,
+                   "min": 0,
+                   "max": 400},
+    "beta_ca3"  : {"active": True,
+                   "color": "green",
+                   "init": 100,
+                   "scale": 100,
+                   "min": 0,
+                   "max": 400},
+    "alpha" : {"active": True,
+               "color": "orange",
+               "init": 0.1,
+               "scale": 1.,
+               "min": 0,
+               "max": 1},
+    "num_swaps_ca1": {"active": False,
+                      "color": "purple",
+                      "init": 1.,
+                      "scale": 1.,
+                      "min": 1,
+                      "max": 50},
+    "num_swaps_ca3": {"active": False,
+                      "color": "purple",
+                      "init": 1.,
+                      "scale": 1.,
+                      "min": 1,
+                      "max": 10}
+}
 
 # --------------------------------------------------
 # --------------------------------------------------
@@ -53,23 +114,33 @@ def fit_population(population: list, datasets: list, settings: dict):
     fitted = []
     for ind in population:
         try:
-            fitted += [[ np.exp(-1. * np.clip(evaluate_genome(ind, datasets, settings), 0., 1.))]]
+            fitted += [[ np.exp(-1. * np.clip(evaluate_genome(ind, datasets, settings),
+                                              0., 1.))]]
         except Exception:
             fitted += [[0.]]
 
     return fitted
 
 
-
 def main(npop: int, ngen: int, num_samples: int=200, num_reps: int=1,
-         save: bool=False):
+         save: bool=False, load_index: int=0, K_data: int=-1,
+         num_lineages: int=2):
 
     # -- settings
-    settings = load_autoencoder(index=0)
+    settings = load_autoencoder(index=load_index)
     settings["num_samples"] = num_samples
     settings["sigma"] = SIGMA
+    settings["genome_configs"] = GENOME_CONFIGS
+    for k, v in settings["genome_configs"].items():
+        settings["genome_configs"][k]["var"] = np.sqrt(
+                settings["genome_configs"][k]["max"] - \
+                settings["genome_configs"][k]["min"])
+
+    # -- dataset
+    if K_data == -1:
+        K_data = int(settings["K_lat"])
     datasets = make_datasets(num_samples=num_samples, num_reps=num_reps,
-                             dim_ei=settings["dim_ei"], K=int(settings["K"]))
+                             dim_ei=settings["dim_ei"], K=K_data)
 
     # -- save
     name = f"run_{len(os.listdir('logs/'))}"
@@ -80,29 +151,42 @@ def main(npop: int, ngen: int, num_samples: int=200, num_reps: int=1,
             "dim_ei": settings["dim_ei"],
             "dim_ca1": settings["dim_ca1"],
             "dim_eo": settings["dim_eo"],
-            "K": settings["K"]}
+            "dim_ca3": settings["genome_configs"]["K_ca3"]["max"],
+            "K": settings["K_lat"]}
 
     # -- evolution setup
-    dim = 5
-    num_lineages = 2
+    dim = 0
+    gene_names = []
+    gene_idx = []
+    for i, (k, v) in enumerate(GENOME_CONFIGS.items()):
+        if v["active"]:
+            gene_names += [k]
+            gene_idx += [i]
+            dim += 1
     size_lineage = npop
-    spawn_rate = 30*6
+    spawn_rate = -1
 
-    settings_ev = ev.EvolutionSettings(num_lineages, size_lineage, spawn_rate, 0.8);
+    settings_ev = ev.EvolutionSettings(num_lineages, size_lineage, spawn_rate, 0.8)
 
     # genes
-    space = np.zeros((dim, 2));
-    for i in range(dim):
-        space[i] = [-0.5, 0.5]
+    # space = np.zeros((dim, 2));
+    # for i in range(dim):
+    #     space[i] = [0.1, 0.4]
 
-    space = [[0.5, 0.5]] * 3
-    space += [[0.5, 0.5], [0.4, 0.4]]
+    means = []
+    variances = []
+    for k, v in settings["genome_configs"].items():
+        means += [v["init"]]
+        variances += [v["var"]]
 
-    evolution = ev.Evolution(settings_ev, space);
+    # space = [[0.5, 0.5]] * 3
+    # space += [[0.5, 0.5], [0.4, 0.4]]
+
+    evolution = ev.Evolution(settings_ev, means, variances)
 
     # plot
     fig, ax = plt.subplots()
-    fig2, ax2 = plt.subplots(dim, num_lineages, sharex=True, sharey=True)
+    fig2, ax2 = plt.subplots(dim, num_lineages, sharex=True, sharey=False)
     record = {"fitness": [], "sigma": SIGMA}
     for _d in range(dim): record[_d] = []
 
@@ -131,7 +215,7 @@ def main(npop: int, ngen: int, num_samples: int=200, num_reps: int=1,
             _pop = []
             record[l][_d] = []
             for _ind in population:
-                _pop += [_ind[_d]]
+                _pop += [_ind[gene_idx[_d]]]
             record[l][_d] += [_pop]
 
     # -- run
@@ -141,6 +225,7 @@ def main(npop: int, ngen: int, num_samples: int=200, num_reps: int=1,
 
         for lin in range(num_lineages):
             population = evolution.update(fitness)
+
             fitness = fit_population(population, datasets, settings)
             _fitness = -1.*np.log(np.array(fitness))
             logger(f"gen={gen+1} | [{lin}] fitness={np.max(_fitness):.3f}")
@@ -155,7 +240,7 @@ def main(npop: int, ngen: int, num_samples: int=200, num_reps: int=1,
             for _d in range(dim):
                 _pop = []
                 for _ind in population:
-                    _pop += [_ind[_d]]
+                    _pop += [_ind[gene_idx[_d]]]
                 record[lin][_d] += [_pop]
 
             record[lin]["history"] += [np.mean(_fitness.flatten()).item()]
@@ -166,21 +251,28 @@ def main(npop: int, ngen: int, num_samples: int=200, num_reps: int=1,
             for h in range(num_lineages):
                 for x, g in enumerate(np.array(record[h]["fitness"])):
                     ax.scatter([x]*len(population), g, s=30, color=record[h]["color"],
-                               marker="x", alpha=0.1)
-                ax.plot(range(len(record[h]["history"])), record[h]["history"], color=record[h]["color"], lw=2)
+                               marker="x", alpha=0.3)
+                ax.plot(range(1, len(record[h]["history"])), record[h]["history"][1:], color=record[h]["color"], lw=2,
+                        label=f"lineage {h}")
             ax.set_xlabel("generations")
             ax.set_ylabel("fitness")
+            ax.legend(loc="upper left")
+            ax.set_ylim((0, 1))
+            ax.set_xlim((1, len(record[0]["history"])))
 
+            # for k in range(dim):
             for k in range(dim):
+                if len(record) == 0: break
                 for h in range(num_lineages):
                     ax2[k][h].clear()
                     ax2[k][h].grid()
                     for x, g in enumerate(np.array(record[h][k])):
                         ax2[k][h].scatter([x]*len(g), g, s=30, c=record[h]['fitness'][x],
                                        cmap="Blues_r", alpha=0.8)
-                    ax2[k][h].set_ylabel(f"gene {k}")
-                    if k == 0:
-                        ax2[k][h].set_title(f"lineage {h}")
+                    # ax2[k][h].set_ylim(minv, maxv)
+                    # ax2[k][h].set_ylabel(f"gene {k}")
+                    if h == 0: ax2[k][h].set_ylabel(f"{gene_names[k]}")
+                    if k == 0: ax2[k][h].set_title(f"lineage {h}")
 
                 # ax2[k][h].set_xlabel("generations")
 
@@ -188,7 +280,7 @@ def main(npop: int, ngen: int, num_samples: int=200, num_reps: int=1,
 
         # save
         if save:
-            info["genome"] = {_n: _g for _n, _g in zip(GENOME_KEYS, best_genome)}
+            info["genome"] = {_n: _g for _n, _g in zip(gene_names, best_genome)}
             info["fitness"] = best_fitness
             # info["record"] = record
             save_genome(info=info, name=name)
@@ -216,10 +308,14 @@ if __name__ == "__main__":
     parser.add_argument('--reps', type=int,
                         help='number of repetitions',
                         default=1)
+    parser.add_argument('--lineages', type=int,
+                        help='number of lineages',
+                        default=2)
     args = parser.parse_args()
 
+
     main(npop=args.pop, ngen=args.gen, num_samples=int(args.samples),
-         num_reps=args.reps, save=args.save)
+         num_reps=args.reps, save=args.save, num_lineages=args.lineages)
 
     logger("[done]")
 
